0e9bebcef6d5f5fd53d83bdbed6a6674dcd9613a
M.M.Hajian <hajianmohammadmahdi582@gmail.com>
2021-11-19 23:25:38 +0330

    Index page updated

---
+++ Atom/resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atom</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <form action="/explore" method="get">
+                    <div>
+                        <input type="text" name="query" id="" placeholder="Please enter atom">
+                    </div>
+                    <div>
+                        <input type="radio" name="param" id="">
+                        <input type="radio" name="param" id="">
+                        <input type="radio" name="param" id="">
+                        <input type="radio" name="param" id="">
+                        <input type="radio" name="param" id="">
+                        <input type="radio" name="param" id="">
+                        <input type="radio" name="param" id="">
+                    </div>
+                    <div>
+                        <button type="submit">Get</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>l
