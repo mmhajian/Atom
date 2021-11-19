30db4dba609c671143b38fbfd08830aa08f4438b
M.M.Hajian <hajianmohammadmahdi582@gmail.com>
2021-11-20 00:25:00 +0330

    Index page updated

--- Atom/resources/views/index.blade.php
+++ Atom/resources/views/index.blade.php
@@ -18,41 +18,41 @@
             </div>
             <div>
                 <form action="/explore" method="get">
-                    <div>
-                        <input type="text" name="query" id="" placeholder="Please enter atom" required>
+                    <div class="div-atom">
+                        <input class="input-atom" type="text" name="query" id="" placeholder="Please enter atom" required>
                     </div>
-                    <div>
-                        <div>
+                    <div class="params">
+                        <div class="param-div">
                             <label for="chb-name">Atom Name</label>
                             <input type="radio" name="param" id="chb-name" value="name" checked>
                         </div>
-                        <div>
+                        <div class="param-div">
                             <label for="chb-number">Atomic Number</label>
                             <input type="radio" name="param" id="chb-number" value="number">
                         </div>
-                        <div>
+                        <div class="param-div">
                             <label for="chb-mass">Atomic Mass</label>
                             <input type="radio" name="param" id="chb-mass" value="mass">
                         </div>
-                        <div>
+                        <div class="param-div">
                             <label for="chb-symbol">Symbol</label>
                             <input type="radio" name="param" id="chb-symbol" value="symbol">
                         </div>
-                        <div>
+                        <div class="param-div">
                             <label for="chb-isotopes">Isotopes</label>
                             <input type="radio" name="param" id="chb-isotopes" value="isotopes">
                         </div>
-                        <div>
+                        <div class="param-div">
                             <label for="chb-neutrons">Neutrons</label>
                             <input type="radio" name="param" id="chb-neutrons" value="neutrons">
                         </div>
-                        <div>
+                        <div class="param-div">
                             <label for="chb-stability">Atom Stability</label>
                             <input type="radio" name="param" id="chb-stability" value="stability">
                         </div>
                     </div>
-                    <div>
-                        <button type="submit">Get</button>
+                    <div class="div-submit">
+                        <button class="btn-submit" type="submit">Get</button>
                     </div>
                 </form>
             </div>
