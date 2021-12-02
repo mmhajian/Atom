89c8dcacb8bb89fcc5a80887224588b40932f109
M.M.Hajian <hajianmohammadmahdi582@gmail.com>
2021-12-03 00:05:03 +0330

    Models updated

--- Atom/app/Models/Atom.php
+++ Atom/app/Models/Atom.php
@@ -4,8 +4,60 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\This;
 
 class Atom extends Model
 {
     use HasFactory;
+
+    //Private
+    private $id;
+
+    private $name;
+
+    private $atomic_number;
+
+    private $atomic_mass;
+
+    private $symbol;
+
+    private $stability;
+
+    private $isotopes;
+
+    private $proton;
+
+    private $electron;
+
+    private $neutron;
+
+
+    //Methods
+
+    //Public Methods
+
+    //Static Methods
+    public function __construct(int $id)
+    {
+        //
+    }
+
+    public static function atom_exists(int $id)
+    {
+        return Atom::where("id", $id)->exists();
+    }
+
+    public static function getAtomNameById(int $id)
+    {
+        //Variables
+        $atom = Atom::where("id", $id);
+
+        //Proccesses
+        if ($atom->exists()) {
+            return $atom->get();
+        }
+        else {
+            return false;
+        }
+    }
 }
